ab20430e0dbb204b0dd972288ee4602daeac6d0f
Diakov Sasha <diakovsasha777@gmail.com>
2022-09-19 23:38:40 +0300

    add scrollUpBtn

---
+++ src/scrollup.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+  let scrollUpParent = document.querySelector('.scroll__up');
+  let scrollUpValue = document.querySelector('.scroll__up--value');
+  let pos = document.documentElement.scrollTop;
+
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollUpParent.style.display = 'flex';
+  } else {
+    scrollUpParent.style.display = 'none';
+  }
+  scrollUpParent.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollUpParent.style.background = `conic-gradient(#add8e6 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
